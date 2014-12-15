21b66f70b692789d2cf82cf678257e1799dc1a76
naulgabriel <naulgabrieldesouza@MacBook-de-naul-gabriel-de-souza.local>
2014-12-14 23:04:23 -0200

    002

---
+++ scripts/template.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$( document ).ready(function() {
+	// Handler for .ready() called.
+	$.Footer({
+		element : '#rodape',
+		setWidth : false
+	});	
+	
+	$('.collapse').collapse();
+	
+	 $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+		
+
+});
