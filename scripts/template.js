15f7cb254329ad8845b8f0f13c1f87e122c133a3
naulgabriel <naulgabrieldesouza@MacBook-de-naul-gabriel-de-souza.local>
2014-12-23 00:43:51 -0200

    004

--- scripts/template.js
+++ scripts/template.js
@@ -8,20 +8,19 @@ $( document ).ready(function() {
 	});
 	
 	
-
-     
-         $window = $(window);
-         $('section[data-type="background"]').each(function(){
-           var $scroll = $(this);                 
+	 $window = $(window);
+    //Captura cada elemento section com o data-type "background"
+    $('section[data-type="background"]').each(function(){
+        var $scroll = $(this);   
+            //Captura o evento scroll do navegador e modifica o backgroundPosition de acordo com seu deslocamento.            
             $(window).scroll(function() {
-              var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-               var coords = '50% '+ yPos + 'px';
-              $scroll.css({ backgroundPosition: coords });    
+                var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+                var coords = '50% '+ yPos + 'px';
+                $scroll.css({ backgroundPosition: coords });    
             });
-         });  
-      
-      
+   });  
 	
-		
+	
+
 
 });
